eabcb74151f920d8ae2e89a3443470f4883216cc
Jozef Kralik <jozef.kralik@eset.sk>
2017-08-22 14:38:43 +0200

    fix of using deprecated NewInstance

--- src/uwrap.cc
+++ src/uwrap.cc
@@ -157,7 +157,7 @@ namespace uwrap {
 				const int argc = 1;
 				v8::Local<v8::Value> argv[argc] = { info[0] };
 				v8::Local<v8::Function> cons = Nan::New(constructor());
-				info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+				info.GetReturnValue().Set(Nan::NewInstance(cons,argc, argv).ToLocalChecked());
 			}
 		}
 
